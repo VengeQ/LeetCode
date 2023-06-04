b713141595691c4e418e65997e78b34a58e06612
Daniil <vengeq@gmail.com>
2023-06-04 14:21:43 +1000

    daily 547

---
+++ LeetCode/NumberOfProvinces.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //547
+    public class NumberOfProvinces
+    {
+        public class Solution
+        {
+            public int FindCircleNum(int[][] isConnected)
+            {
+                var visited = isConnected.Select(x => false).ToList();
+                var queue = new Queue<int>();
+                var provinceCounter = 0;
+
+                while (visited.Any(v => v == false))
+                {
+                    queue.Enqueue(visited.IndexOf(false));
+                    provinceCounter++;
+
+                    while (queue.Count > 0)
+                    {
+                        var city = queue.Dequeue();
+                        visited[city] = true;
+
+                        var neighbours = isConnected[city].Select((item, index) => (item, index)).Where(x => x.item == 1 && x.index != city);
+
+                        foreach (var item in neighbours)
+                        {
+                            if (visited[item.index] == false)
+                            {
+                                visited[item.index] = true;
+                                queue.Enqueue(item.index);
+                            }
+                        }
+
+                    }
+                }
+                return provinceCounter;
+            }
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,20 +2,37 @@
 using LeetCode;
 using System.Diagnostics;
 
-int n = 6;
-int headID = 2;
-var manager = new int[] { 2, 2, -1, 2, 5, 2 };
-var informTime = new int[] { 0, 0, 1, 0, 0, 2 };
+var data = new int[][] {
+    new int[]{1, 1, 0},
+    new int[]{1, 1, 0},
+    new int[]{0 ,0 ,1 }
+};
+
+var data2 = new int[][] {
+    new int[]{1, 0, 0},
+    new int[]{0, 1, 0},
+    new int[]{0 ,0 ,1 }
+};
+
+var data3 = new int[][] {
+    new int[]{1,0,0,1},
+    new int[]{0,1,1,0},
+    new int[]{ 0, 1, 1, 1 },
+    new int[]{ 1, 0, 1, 1 }
+};
+
+
 
 //int n = 1;
 //int headID = 0;
 //var manager = new int[] { -1};
 //var informTime = new int[] { 0 };
 
-var tn = new TimeNeededToInformAllEmployees.Solution();
+var tn = new NumberOfProvinces.Solution();
 
-Console.WriteLine(tn.NumOfMinutes(n, headID, manager, informTime));
 
-//var converted = tn.Convert(n, headID, manager, informTime);
+Console.WriteLine(tn.FindCircleNum(data));
 
-//Console.WriteLine(converted);
+Console.WriteLine(tn.FindCircleNum(data2));
+
+Console.WriteLine(tn.FindCircleNum(data3));
