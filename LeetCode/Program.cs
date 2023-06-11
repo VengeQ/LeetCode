87dc72ed8fd61bf405e8f7a5b14a0d39147e2871
Daniil <vengeq@gmail.com>
2023-06-11 18:11:23 +1000

    daily 1146

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,39 @@
-﻿
-using LeetCode;
-using System.Collections;
+﻿using LeetCode;
 
-var flips = new MinimumFlips.Solution();
+var x = new SnapshotArray(1);
+x.Set(0, 4);
+x.Set(0, 16);
+x.Set(0, 13);
+Console.WriteLine(x.Snap());
+x.Get(0, 0);
+Console.WriteLine(x.Snap());
 
-Console.WriteLine(flips.MinFlips(2, 6, 5));
+var y = new SnapshotArray(3);
+y.Set(0, 5);
+Console.WriteLine(y.Snap());
+y.Set(0, 6);
+y.Set(0, 13);
+y.Get(0, 0);
+Console.WriteLine(y.Get(0, 0));
 
-Console.WriteLine(flips.MinFlips(4, 2, 7));
 
-Console.WriteLine(flips.MinFlips(1, 2, 3));
+//var z = new SnapshotArray(3);
+//z.Set(1, 6);
+//Console.WriteLine(z.Snap());
+//Console.WriteLine(z.Snap());
+//z.Set(1, 19);
+//z.Set(0, 4);
+//Console.WriteLine(z.Get(2, 1));
+//Console.WriteLine(z.Get(2, 0));
+//Console.WriteLine(z.Get(0, 1));
+
+
+var z = new SnapshotArray(3);
+z.Set(1, 6);
+Console.WriteLine(z.Snap());
+Console.WriteLine(z.Snap());
+z.Set(1, 19);
+z.Set(0, 4);
+Console.WriteLine(z.Get(2, 1));
+Console.WriteLine(z.Get(2, 0));
+Console.WriteLine(z.Get(0, 1));
---
+++ LeetCode/SnapshotArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    // №1146
+    public class SnapshotArray
+    {
+        private SortedList<int, int>[] Snapshots;
+        private int SnapCounter = 0;
+
+        public SnapshotArray(int length)
+        {
+            Snapshots = new SortedList<int, int>[length];
+            for (int i = 0; i < length; i++)
+            {
+                Snapshots[i] = new SortedList<int, int>();
+                Snapshots[i].Add(0, 0);
+            }
+        }
+
+        public void Set(int index, int val)
+        {
+            Snapshots[index][SnapCounter] = val;
+        }
+
+        public int Snap()
+        {
+            return SnapCounter++;
+        }
+
+        public int Get(int index, int snap_id)
+        {
+            var dictionary = Snapshots[index];
+
+            if (dictionary.TryGetValue(snap_id, out var val))
+            {
+                return val;
+            }
+            else
+            {
+                return dictionary.Where(key => key.Key<= snap_id).Last().Value;
+            }
+
+        }
+    }
+}
