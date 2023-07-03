510cce46c55c01c2813ecd6feb948851ede36bd7
Daniil <vengeq@gmail.com>
2023-07-04 00:49:39 +1000

    Рефакторинг

--- DynamicProgrammingTests/FibonacciTests.cs
+++ DynamicProgrammingTests/FibonacciTests.cs
@@ -5,25 +5,12 @@ namespace DynamicProgrammingTests
 {
     public class FibonacciTests
     {
-        private FibonacciNumber.Solution? _fibonacciNumberSolution;
-
-        [SetUp]
-        public void Setup()
-        {
-            _fibonacciNumberSolution = new FibonacciNumber.Solution();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            _fibonacciNumberSolution = null;
-        }
-
         [Test]
         public void FibonacciNumber_InputValue2_Returns1()
         {
             var expected = 1;
-            var actual = _fibonacciNumberSolution!.Fib(2);
+
+            var actual = CreateSolution().Fib(2);
 
             Assert.AreEqual(expected, actual);
         }
@@ -32,7 +19,8 @@ namespace DynamicProgrammingTests
         public void FibonacciNumber_InputValue3_Returns2()
         {
             var expected = 2;
-            var actual = _fibonacciNumberSolution!.Fib(3);
+
+            var actual = CreateSolution().Fib(3);
 
             Assert.AreEqual(expected, actual);
         }
@@ -41,9 +29,12 @@ namespace DynamicProgrammingTests
         public void FibonacciNumber_InputValue4_Returns3()
         {
             var expected = 3;
-            var actual = _fibonacciNumberSolution!.Fib(4);
+
+            var actual = CreateSolution().Fib(4);
 
             Assert.AreEqual(expected, actual);
         }
+
+        private static FibonacciNumber.Solution CreateSolution() => new();
     }
 }
--- DynamicProgrammingTests/TribonacciTests.cs
+++ DynamicProgrammingTests/TribonacciTests.cs
@@ -6,55 +6,51 @@ namespace DynamicProgrammingTests
 {
     public class TribonacciTests
     {
-        private TribonacciNumber.Solution? _tribonacciNumberSolution;
-
-        [SetUp]
-        public void Setup()
-        {
-            _tribonacciNumberSolution = new TribonacciNumber.Solution();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            _tribonacciNumberSolution = null;
-        }
-
-        [Test]
         [TestCase(-100)]
         [TestCase(-1)]
         [TestCase(int.MinValue)]
-        public void Tribonacci_WhenInputLessThanZero_ShouldThrowException(int input)
+        [Category("ExceptionTest")]
+        public void Tribonacci_WhenInputLessThanZero_ShouldThrowArgumentException(int input)
         {
-            var exception = Assert.Throws<ArgumentException>(() => _tribonacciNumberSolution!.Tribonacci(input));
+            var exception = Assert.Throws<ArgumentException>(() => CreateSolution().Tribonacci(input));
+
             Assert.That(exception!.Message, Does.StartWith("n should be between 0 and 37"));
         }
 
-        [Test]
         [TestCase(38)]
         [TestCase(80)]
         [TestCase(int.MaxValue)]
-        public void Tribonacci_WhenInputMoreThan37_ShouldThrowException(int input)
+        [Category("ExceptionTest")]
+        public void Tribonacci_WhenInputMoreThan37_ShouldThrowArgumentException(int input)
         {
-            var exception = Assert.Throws<ArgumentException>(() => _tribonacciNumberSolution!.Tribonacci(input));
+            var exception = Assert.Throws<ArgumentException>(() => CreateSolution().Tribonacci(input));
+
             Assert.That(exception!.Message, Does.StartWith("n should be between 0 and 37"));
         }
 
         [Test]
+        [Category("GoodInputTest")]
         public void Tribonacci_InputValue4_Returns4()
         {
             var expected = 4;
-            var actual = _tribonacciNumberSolution!.Tribonacci(4);
+
+            var actual = CreateSolution().Tribonacci(4);
+
             Assert.That(actual, Is.EqualTo(expected));
         }
 
 
         [Test]
-        public void Tribonacci_InputValue25_Returns4()
+        [Category("GoodInputTest")]
+        public void Tribonacci_InputValue25_Returns1389537()
         {
             var expected = 1389537;
-            var actual = _tribonacciNumberSolution!.Tribonacci(25);
+
+            var actual = CreateSolution().Tribonacci(25);
+
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        private static TribonacciNumber.Solution CreateSolution() => new();
     }
 }
