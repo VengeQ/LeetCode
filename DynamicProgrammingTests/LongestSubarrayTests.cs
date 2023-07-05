a3c6e76f2b8a585ea0d84206c71367ff11b364eb
Daniil <vengeq@gmail.com>
2023-07-05 15:29:28 +1000

    Daily задачка на Sliding Window

---
+++ DynamicProgrammingTests/LongestSubarrayTests.cs
@@ -0,0 +1,64 @@
+﻿using LeetCode.DynamicProgramming;
+using NUnit.Framework;
+using System;
+
+namespace DynamicProgrammingTests
+{
+    public class LongestSubarrayTests
+    {
+
+        [Test]
+        public void LongestSubarray_ArrayZeroLength_ThrowException()
+        {
+            var solution = CreateSolution();
+
+            var ex = Assert.Throws<ArgumentException>(() => solution.LongestSubarray(Array.Empty<int>()));
+            Assert.That(ex!.Message, Does.StartWith("Длина массива должна быть больше 0"));
+        }
+
+        [Test]
+        public void LongestSubarray_ArrayLengthMoreThan100000_ThrowException ()
+        {
+            var solution = CreateSolution();
+
+            var ex = Assert.Throws<ArgumentException>(() => solution.LongestSubarray(new int[100001]));
+            Assert.That(ex!.Message, Does.StartWith("Длина массива не должна превышать 100000"));
+        }
+
+
+        [Test]
+        public void LongestSubarray_NullInputArray_ThrowException()
+        {
+            var solution = CreateSolution();
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            var ex = Assert.Throws<ArgumentNullException>(() => solution.LongestSubarray(null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            Assert.That(ex!.Message, Does.StartWith("Входной параметр не должен быть равен нулю"));
+        }
+
+        [TestCase(new int[] { 1, 1, 0, 1 }, 3)]
+        [TestCase(new int[] { 0, 1, 1, 1, 0, 1, 1, 0, 1 }, 5)]
+        [TestCase(new int[] { 1, 1, 1 }, 2)]
+        public void LongestSubarray_ValidInputArray_ReturnLongestSubarraySize(int[] input, int expectedResult)
+        {
+            var actual = CreateSolution().LongestSubarray(input);
+
+            Assert.That(actual, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 0, 1, int.MaxValue })]
+        [TestCase(new int[] { 0, 1, -1 })]
+        [TestCase(new int[] { 1, 1, int.MinValue })]
+        public void LongestSubarray_NotZeroOrNotOneElementInArray_ThrowException(int[] input)
+        {
+            var solution = CreateSolution();
+
+            var ex = Assert.Throws<ArgumentException>(() => solution.LongestSubarray(input));
+            Assert.That(ex!.Message, Does.StartWith("Во входном массиве все элементы должны быть 0 или 1"));
+        }
+
+        private static LongestSubarray.Solution CreateSolution() => new();
+    }
+}
---
+++ LeetCode/DynamicProgramming/LongestSubarray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DynamicProgramming
+{
+    // №1493
+    public class LongestSubarray
+    {
+        public class Solution
+        {
+            public int LongestSubarray(int[] nums)
+            {
+                if (nums == null)
+                {
+                    throw new ArgumentNullException(nameof(nums), "Входной параметр не должен быть равен нулю");
+                }
+
+                if (nums.Length == 0)
+                {
+                    throw new ArgumentException("Длина массива должна быть больше 0");
+                }
+
+                if (nums.Length > 100000)
+                {
+                    throw new ArgumentException("Длина массива не должна превышать 100000");
+                }
+
+                if (nums.Any(x => x != 0 && x != 1))
+                {
+                    throw new ArgumentException("Во входном массиве все элементы должны быть 0 или 1");
+                }
+
+                if (nums.Length == 1)
+                {
+                    return nums[0];
+                }
+
+                var result = 0;
+                var current = 0;
+                var inseparablePart = 0;
+
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == 0)
+                    {
+                        result = Math.Max(result, current);
+                        current = inseparablePart;
+                        inseparablePart = 0;                       
+                    }
+                    else
+                    {
+                        current++;
+                        inseparablePart++;
+                    }                   
+                }
+
+                result = Math.Max(result, current);
+                return Math.Min(result, nums.Length - 1);
+            }
+        }
+    }
+}
