7e2ac4df901755494ccc41eed779fafab2ac0a4f
Daniil <vengeq@gmail.com>
2023-07-06 00:06:20 +1000

    + One dynamic programming problem

---
+++ DynamicProgrammingTests/MinCostClimbingStairsTests.cs
@@ -0,0 +1,54 @@
+﻿using LeetCode.DynamicProgramming;
+using NUnit.Framework;
+using System;
+
+namespace DynamicProgrammingTests
+{
+    [TestFixture]
+    public class MinCostClimbingStairsTests
+    {
+        [Test]
+        public void MinimalCost_ArrayLengthLessThan2_ThrowException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>CreateSolution().MinCostClimbingStairs(new int[] { 1 }));
+
+            Assert.That(ex!.Message, Does.StartWith("Длина входного массива должна быть больше 1."));
+        }
+
+        [Test]
+        public void MinimalCost_ArrayLengthMoreThan1000_ThrowException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => CreateSolution().MinCostClimbingStairs(new int[1001]));
+
+            Assert.That(ex!.Message, Does.StartWith("Длина входного массива должна быть меньше 1000."));
+        }
+
+        [Test]
+        public void MinimalCost_ArrayLengthEqualTwo_ShouldPass()
+        {
+            CreateSolution().MinCostClimbingStairs(new int[2]);
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void MinimalCost_ArrayLengthEqual1000_ShouldPass()
+        {
+            CreateSolution().MinCostClimbingStairs(new int[1000]);
+
+            Assert.Pass();
+        }
+
+        [TestCase(new int[] { 10, 15, 20 }, 15)]
+        [TestCase(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6)]
+        public void MinimalCost_ValidInputArray_ShouldReturnMinimalCost(int[] input, int result)
+        {
+            var minimalCost = CreateSolution().MinCostClimbingStairs(input);
+
+            Assert.That(result, Is.EqualTo(minimalCost));
+        }
+
+
+        private static MinCostClimbingStairs.Solution CreateSolution() => new();
+    }
+}
---
+++ LeetCode/DynamicProgramming/MinCostClimbingStairs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DynamicProgramming
+{
+    // № 746
+    public class MinCostClimbingStairs
+    {
+        public class Solution
+        {
+            public int MinCostClimbingStairs(int[] cost)
+            {
+                if (cost.Length < 2)
+                {
+                    throw new ArgumentException("Длина входного массива должна быть больше 1.");
+                }
+
+                if (cost.Length > 1000)
+                {
+                    throw new ArgumentException("Длина входного массива должна быть меньше 1000.");
+                }
+
+                int temporary;
+                var twoStepBehind = cost[0];
+                var oneStepBehind = cost[1];
+
+                for (int i = 2; i < cost.Length; i++)
+                {
+                    temporary = Math.Min(twoStepBehind + cost[i], oneStepBehind + cost[i]);
+                    twoStepBehind = oneStepBehind;
+                    oneStepBehind = temporary;
+                }
+
+                return Math.Min(twoStepBehind, oneStepBehind);
+            }
+        }
+    }
+}
